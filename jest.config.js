6ec526a3d5198f41e1d28697eb620722f4acdc03
Dmitbd <105349881+Dmitbd@users.noreply.github.com>
2026-08-13 07:45:27 +0300

    chore: усилить проверки проекта

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "jest-expo",
   setupFiles: ["<rootDir>/jest.setup.js"],
-  testPathIgnorePatterns: ["/node_modules/", "/.expo/"],
+  testPathIgnorePatterns: ["/node_modules/", "/.expo/", "<rootDir>/.worktrees/"],
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|expo-router|@expo/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg))",
   ],
